171ce014e3ac3855ff52d76fc1d5a0e4f399af64
Javier Figueroa Mayordomo <javier.roamayor@gmail.com>
2022-10-21 12:00:31 +0200

    Updated Maggie Level exercise

---
+++ SimpsonsExercise/main/maggie_level.py
@@ -0,0 +1,53 @@
+from urllib import response
+import pip._vendor
+from pip._vendor import requests
+import time
+import csv
+import json
+
+response = requests.get("https://thesimpsonsquoteapi.glitch.me/quotes")
+
+quotes_dict = {"character": "Homer", "quote": "Doh!"}
+
+def simpsons_quotes():
+  while True:
+    response = requests.get("https://thesimpsonsquoteapi.glitch.me/quotes")
+    json_file = response.json()[0]
+    if(json_file['character'] == "Lisa Simpson"):
+      with open('/Users/javierfm/Documents/GitHub/Javier_EDEM2022/SimpsonsExercise/main/lisa_quotes.csv', 'a') as Lisa:
+        quotes_dict = {'character': json_file['character'], 'quote': json_file['quote']}
+        w = csv.DictWriter(Lisa, quotes_dict.keys())
+        w.writerow(quotes_dict)
+      with open('/Users/javierfm/Documents/GitHub/Javier_EDEM2022/SimpsonsExercise/main/general.csv', 'a') as General:
+        w2 = csv.DictWriter(General, quotes_dict.keys())
+        w2.writerow(quotes_dict)
+    elif(json_file['character'] == "Homer Simpson"):
+      with open('/Users/javierfm/Documents/GitHub/Javier_EDEM2022/SimpsonsExercise/main/homer_quotes.csv', 'a') as Homer:
+        quotes_dict = {'character': json_file['character'], 'quote': json_file['quote']}
+        w3 = csv.DictWriter(Homer, quotes_dict.keys())
+        w3.writerow(quotes_dict)
+      with open('/Users/javierfm/Documents/GitHub/Javier_EDEM2022/SimpsonsExercise/main/general.csv', 'a') as General:
+        w4 = csv.DictWriter(General, quotes_dict.keys())
+        w4.writerow(quotes_dict)   
+    else:
+      with open('/Users/javierfm/Documents/GitHub/Javier_EDEM2022/SimpsonsExercise/main/general.csv', 'a') as General:
+        quotes_dict = {'character': json_file['character'], 'quote': json_file['quote']}
+        w5 = csv.DictWriter(General, quotes_dict.keys())
+        w5.writerow(quotes_dict)
+    print(quotes_dict)
+    time.sleep(30)
+    
+simpsons_quotes()
+
+  
+  
+
+
+  
+
+
+
+
+
+
+
