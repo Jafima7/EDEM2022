5e46d549c3f3f89c86c79e5e23fee4951a633c4b
Javier Figueroa Mayordomo <javier.roamayor@gmail.com>
2022-10-22 19:04:01 +0200

    Updated Lisa Level

--- SimpsonsExercise/main/lisa_level.py
+++ SimpsonsExercise/main/lisa_level.py
@@ -10,7 +10,7 @@ import numpy as np
 import cv2
 
 def url_to_image(url):
-  resp = urllib.urlopen(url)
+  resp = urllib.requests.urlopen(url)
   image = np.asarray(bytearray(resp.read()), dtype="uint8")
   image = cv2.imdecode(image, cv2.IMREAD_COLOR)
   return image
